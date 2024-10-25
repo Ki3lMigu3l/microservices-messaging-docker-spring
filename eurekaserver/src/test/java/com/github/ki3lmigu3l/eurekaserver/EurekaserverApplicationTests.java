9d992e08e0671ea2368e4039d6e0f9ca1910401c
Ezequiel <ki3lmigu3l@gmail.com>
2024-10-25 14:39:43 -0300

    Fisert Commit: add Eureka Server

---
+++ eurekaserver/src/test/java/com/github/ki3lmigu3l/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ki3lmigu3l.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
