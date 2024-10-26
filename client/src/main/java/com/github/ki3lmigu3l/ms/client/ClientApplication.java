17eaa545b5b2af4fd9719fd2ea2002503c2a2c6c
Ezequiel <ki3lmigu3l@gmail.com>
2024-10-25 22:57:14 -0300

    feature: ms-client eureka cliente adicionado ao projeto

---
+++ client/src/main/java/com/github/ki3lmigu3l/ms/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.ki3lmigu3l.ms.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+}
